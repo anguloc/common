e5327df180cef0e9f549f6ee4746ecfea6a54fd4
gk <xy62201793@163.com>
2019-09-28 21:51:39 +0800

    config

---
+++ config.example/Constants.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 常量配置文件
+ */
+
+// host
+defined('HOST_1') || define('HOST_1', '127.0.0.1');
+
+// port
+defined('PORT_1') || define('PORT_1', 3306);
+defined('PORT_2') || define('PORT_2', 6379);
+defined('PORT_3') || define('PORT_3', 5672);
+defined('PORT_4') || define('PORT_4', 11211);
+defined('PORT_5') || define('PORT_5', 9001);
+defined('PORT_6') || define('PORT_6', 9501);
+
+// username
+defined('USER_1') || define('USER_1', '');
+defined('USER_2') || define('USER_2', '');
+defined('USER_3') || define('USER_3', '');
+defined('USER_4') || define('USER_4', '');
+defined('USER_5') || define('USER_5', '');
+defined('USER_6') || define('USER_6', '');
+
+// password
+defined('PWD_1') || define('PWD_1', '');
+defined('PWD_2') || define('PWD_2', '');
+defined('PWD_3') || define('PWD_3', '');
+defined('PWD_4') || define('PWD_4', '');
+defined('PWD_5') || define('PWD_5', '');
+defined('PWD_6') || define('PWD_6', '');
