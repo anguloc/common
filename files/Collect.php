0d95641aac5ac771fd54d93e5431f3a15bb14398
gk <xy62201793@163.com>
2020-07-23 01:25:04 +0800

    push

--- files/Collect.php
+++ files/Collect.php
@@ -12,20 +12,38 @@ if (!function_exists('base32_encode')) {
         // base32 A - Z  2 - 7  ->  65 - 90  50 - 55
         $output = '';
 
-        echo ord('7');
 
+
+
+        $len = 0;
         $bit = 0;
-        $a = $b=$c=$d=$e  = [];
-        for ($i=0;$i<strlen($input);$i++){
+        $a = $b = $c = $d = $e = [];
+        for ($i = 0; $i < strlen($input); $i++) {
+            $bit <<= 8;
             $a[] = $ascii = ord($input[$i]);
 //        $output .= $ascii & 0xf8; // 拿前5位
 
-            $b[] = $ascii&0xf8;
-            $c[] = $ascii&0x7;
-            $d[] = $ascii>>3;
+            $b[] = $ascii & 0xf8;
+            $c[] = $ascii & 0x7;
+            $f = $ascii >> 3;
+            $s = $ascii << 5;
+
+            if ($f < 26) {
+
+            } elseif ($f >= 26 && $f <= 31) {
+
+            } else {
+                return false;
+            }
 
 
-//        $bit .=
+            $len += 8;
+            // 这里可能会循环两次
+            while($bit >= 5){
+                $bit -= 5;
+
+            }
+
 
         }
 
@@ -36,6 +54,32 @@ if (!function_exists('base32_encode')) {
 
         return $output;
     }
+}
+
+function asd($input){
+    $BASE32_ALPHABET = 'abcdefghijklmnopqrstuvwxyz234567';
+    $output = '';
+    $v = 0;
+    $vbits = 0;
+
+    for ($i = 0, $j = strlen($input); $i < $j; $i++) {
+        $v <<= 8;
+        $v += ord($input[$i]);
+        $vbits += 8;
+
+        while ($vbits >= 5) {
+            $vbits -= 5;
+            $output .= $BASE32_ALPHABET[$v >> $vbits];
+            $v &= ((1 << $vbits) - 1);
+        }
+    }
+
+    if ($vbits > 0) {
+        $v <<= (5 - $vbits);
+        $output .= $BASE32_ALPHABET[$v];
+    }
+
+    return $output;
 }
 
 if (!function_exists('base32_decode')) {
